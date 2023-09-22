dedfe66c42bfbca6af5d669b154084f6c7c2e5cd
tanyangyang <tanyangyang@cultraview.com>
2023-09-22 14:43:09 +0800

    基础类以及资源文件的添加

--- app/src/main/java/com/ctv/utils/MainActivity.kt
+++ app/src/main/java/com/ctv/utils/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.ctv.utils
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.ctv.utils.dialog.IosDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IosDialog.CancelListener, IosDialog.ConfirmListener {
+    private var context :Context?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        context = this
+        initData()
+    }
+
+    private fun initData() {
+        val button1 = findViewById<Button>(R.id.dialog_button)
+        button1.setOnClickListener {
+            val iosDialog = IosDialog(context,this,this)
+            iosDialog.show()
+        }
+    }
+
+    override fun remoteControlDialogConfirm() {
+        Toast.makeText(context,R.string.ios_dialog_tip_one,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun remoteControlDialogCancel() {
+        Toast.makeText(context,R.string.ios_dialog_tip_two,Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/ctv/utils/dialog/IosDialog.kt
@@ -0,0 +1,69 @@
+package com.ctv.utils.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.widget.TextView
+import android.os.Bundle
+import android.view.View
+import com.ctv.utils.R
+
+class IosDialog(
+    context: Context?,
+    cancelListener: CancelListener,
+    confirmListener: ConfirmListener,
+    private val dContext: Context? = context
+) : Dialog(
+    context!!), View.OnClickListener {
+    private var dialogTitle: TextView? = null
+    private var dialogContext: TextView? = null
+    private var dialogCancel: TextView? = null
+    private var dialogConfirm: TextView? = null
+    private var confirmListener: ConfirmListener? = confirmListener
+    private var cancelListener: CancelListener? = cancelListener
+
+
+    interface ConfirmListener {
+        fun remoteControlDialogConfirm()
+    }
+
+    interface CancelListener {
+        fun remoteControlDialogCancel()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ios_dialog)
+        initViews()
+    }
+
+    override fun show() {
+        super.show()
+        dialogTitle!!.text =  dContext?.getString(R.string.ios_dialog_tip)
+        dialogContext!!.text = dContext?.getString(R.string.ios_dialog_context)
+        dialogCancel!!.text = dContext?.getString(R.string.ios_dialog_cancel)
+        dialogConfirm!!.text = dContext?.getString(R.string.ios_dialog_confirm)
+    }
+
+    private fun initViews() {
+        dialogTitle = findViewById(R.id.dialog_title)
+        dialogContext = findViewById(R.id.dialog_context)
+        dialogCancel = findViewById(R.id.dialog_cancel)
+        dialogConfirm = findViewById(R.id.dialog_confirm)
+        dialogCancel?.setOnClickListener(this)
+        dialogConfirm?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.dialog_confirm -> {
+                confirmListener!!.remoteControlDialogConfirm()
+                dismiss()
+            }
+            R.id.dialog_cancel -> {
+                cancelListener!!.remoteControlDialogCancel()
+                dismiss()
+            }
+            else -> {}
+        }
+    }
+}
